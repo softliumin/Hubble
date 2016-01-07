da3054295ba6d325fb0fc6d9b6dd9e628c053174
lizhitao <304663257@qq.com>
2016-01-07 23:04:31 +0800

    增加 ThreadLocal 缓存

---
+++ src/main/java/cc/sharper/sword/cache/support/ThreadLocalCache.java
@@ -0,0 +1,34 @@
+package cc.sharper.sword.cache.support;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cc.sharper.sword.cache.Cache;
+
+/**
+ * 
+ * @author lizhitao 线程私有的缓存,没有并发问题
+ */
+public class ThreadLocalCache implements Cache {
+	private final ThreadLocal<Map<Object, Object>> cache;
+
+	public ThreadLocalCache() {
+		cache = new ThreadLocal<Map<Object, Object>>() {
+			@Override
+			protected Map<Object, Object> initialValue() {
+				return new HashMap<Object, Object>();
+			}
+		};
+	}
+
+	@Override
+	public void put(Object key, Object value) {
+		cache.get().put(key, value);
+	}
+
+	@Override
+	public Object get(Object key) {
+		return cache.get().get(key);
+	}
+
+}
