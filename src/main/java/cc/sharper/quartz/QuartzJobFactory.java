71cfee878c37f5ccf6a6f53081d65bbe6bcc4d0d
sharper <softliumin@gmail.com>
2016-01-23 20:10:37 +0800

    增加日志

--- src/main/java/cc/sharper/quartz/QuartzJobFactory.java
+++ src/main/java/cc/sharper/quartz/QuartzJobFactory.java
@@ -4,6 +4,8 @@ import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by liumin3 on 2016/1/22.
@@ -12,12 +14,13 @@ import org.quartz.JobExecutionException;
 //@DisallowConcurrentExecution
 public class QuartzJobFactory implements Job
 {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException
     {
-        System.out.println("任务成功运行");
+        log.info("任务成功运行");
         ScheduleJob scheduleJob = (ScheduleJob)context.getMergedJobDataMap().get("scheduleJob");
-        System.out.println("任务名称 = [" + scheduleJob.getJobName() + "]");
+        log.info("任务名称 = [" + scheduleJob.getJobName() + "]");
     }
 }
