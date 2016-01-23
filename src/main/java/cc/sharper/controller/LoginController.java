1c30c16d320740cf00ce1f6cb327f47b3cb591ae
sharper <softliumin@gmail.com>
2016-01-23 20:40:44 +0800

    完善动态worker

--- src/main/java/cc/sharper/controller/LoginController.java
+++ src/main/java/cc/sharper/controller/LoginController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.UUID;
