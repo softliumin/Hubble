73a29609099bf3e5e17e35166c27425bfdb3156b
tiantangyu69 <304663257@qq.com>
2016-03-04 11:34:35 +0800

    测试对象池

--- src/test/java/pool/PoolTest.java
+++ src/test/java/pool/PoolTest.java
@@ -12,35 +12,27 @@ import java.util.concurrent.Executors;
 public class PoolTest {
     public static void main(String[] args) {
         GenericObjectPoolConfig config = new GenericObjectPoolConfig();
-        config.setMaxIdle(10);
-        config.setMaxTotal(30);
+        config.setMaxIdle(5);
+        config.setMaxTotal(10);
 
         final ConnectionPoolFactory poolFactory = new ConnectionPoolFactory("127.0.0.1", 8080, config);
-        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        ExecutorService threadPool = Executors.newFixedThreadPool(30);
 
-        threadPool.execute(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 100; i++) {
+            threadPool.execute(new Runnable() {
+                @Override
+                public void run() {
                     Socket socket = poolFactory.getConnection();
                     System.out.println(socket);
-                }
-            }
-        });
-
-        /*threadPool.execute(new Runnable() {
-            @Override
-            public void run() {
-                while (true){
-                    poolFactory.releaseConnection();
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    poolFactory.releaseConnection(socket);
                 }
-            }
-        });*/
+            });
+        }
 
         threadPool.shutdown();
     }
